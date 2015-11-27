934aaf3972eb06a1fa7c110148993e47d4bf324c
Tiago Barros <tiago.barros@cesar.org.br>
2015-11-27 15:46:55 -0300

    Abstract driver implementation

---
+++ src/abstract_driver.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2015, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include <abstract_driver.h>
+#include <knot_proto_net.h>
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+
+#define EMPTY_DRIVER_NAME	"Empty driver"
+
+int empty_probe()
+{
+	return KNOT_NET_ERROR;
+}
+
+void empty_remove()
+{
+
+}
+
+int empty_socket()
+{
+	return KNOT_SOCKET_FD_INVALID;
+}
+
+void empty_close(int socket)
+{
+
+}
+
+int empty_connect(int socket, uint8_t to_addr)
+{
+	return KNOT_NET_ERROR;
+}
+
+int empty_generic_function(int socket)
+{
+	return KNOT_NET_ERROR;
+}
+
+size_t empty_recv (int sockfd, void *buffer, size_t len)
+{
+	return 0;
+}
+
+size_t empty_send (int sockfd, const void *buffer, size_t len)
+{
+	return 0;
+}
+
+abstract_driver driver_empty = {
+	.name = EMPTY_DRIVER_NAME,
+	.valid = 0,
+	.probe = empty_probe,
+	.remove = empty_remove,
+	.socket = empty_socket,
+	.close = empty_close,
+	.listen = empty_generic_function,
+	.accept = empty_generic_function,
+	.connect = empty_connect,
+	.available = empty_generic_function,
+	.recv = empty_recv,
+	.send = empty_send
+};
+
+#ifdef HAVE_SERIAL
+extern abstract_driver driver_serial;
+#endif
+
+#ifdef HAVE_NRF24
+extern abstract_driver driver_nrf24;
+#endif
+
+#ifdef HAVE_NRF51
+extern abstract_driver driver_nrf51;
+#endif
+
+#ifdef HAVE_NRF905
+extern abstract_driver driver_nrf905;
+#endif
+
+#ifdef HAVE_RFM69
+extern abstract_driver driver_rfm69;
+#endif
+
+#ifdef HAVE_ETH
+extern abstract_driver driver_eth;
+#endif
+
+#ifdef HAVE_ESP8266
+extern abstract_driver driver_esp8266;
+#endif
+
+#ifdef HAVE_ASK
+extern abstract_driver driver_ask;
+#endif
+
+// This struct MUST have the same element order as knot_phy_t
+abstract_driver *drivers[] = {
+#ifdef HAVE_SERIAL
+	&driver_serial,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_NRF24
+	&driver_nrf24,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_NRF51
+	&driver_nrf51,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_NRF905
+	&driver_nrf905,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_RFM69
+	&driver_rfm69,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_ETH
+	&driver_eth,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_ESP8266
+	&driver_esp8266,
+#else
+	&driver_empty,
+#endif
+
+#ifdef HAVE_ASK
+	&driver_ask,
+#else
+	&driver_empty,
+#endif
+	NULL
+};
+
---
+++ src/abstract_driver.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#ifndef ABSTRACT_DRIVER_H
+#define ABSTRACT_DRIVER_H
+
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+
+ /**
+ * struct abstract_driver - driver abstraction for the physical layer
+ * @name: driver name
+ * @probe: function to initialize the driver
+ * @remove: function to close the driver and free its resources
+ * @socket: function to create a new socket FD
+ * @listen: function to enable incoming connections
+ * @accept: function that waits for a new connection and returns a 'pollable' FD
+ * @connect: function to connect to a server socket
+ *
+ * This 'driver' intends to be an abstraction for Radio technologies or
+ * proxy for other services using TCP or any socket based communication.
+ */
+
+typedef struct {
+	const char *name;
+	const uint8_t valid;
+	int (*probe) (void);
+	void (*remove) (void);
+
+	int (*socket) (void);
+	void (*close) (int sockfd);
+	int (*listen) (int srv_sockfd);
+	int (*accept) (int srv_sockfd);
+	int (*connect) (int cli_sockfd, uint8_t to_addr);
+	int (*available) (int sockfd);
+	size_t (*recv) (int sockfd, void *buffer, size_t len);
+	size_t (*send) (int sockfd, const void *buffer, size_t len);
+} abstract_driver;
+
+extern abstract_driver driver_empty;
+extern abstract_driver *drivers[];
+
+#endif // ABSTRACT_DRIVER_H
