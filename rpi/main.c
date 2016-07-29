303158e07b012611b81c494dbbaa08b91e178b7e
Claudio Takahasi <ckt@cesar.org.br>
2016-07-29 13:24:50 -0300

    rpi: Add initial sources for SPI passthrough
    SPI passthrough intends to forward the RAW SPI traffic over a TCP
    socket allowing to develop and debug KNOT HAL on an external host.

---
+++ rpi/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("RPi SPI passthrough over TCP\n");
+
+	return 0;
+}
