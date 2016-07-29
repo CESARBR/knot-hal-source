1d0f05220d6e9fc84e960dc59276babd69e78e48
Claudio Takahasi <ckt@cesar.org.br>
2016-07-29 14:46:04 -0300

    rpi: Add 'port' option to expose nRF24L01 SPI
    This patch adds the a command line option to inform TCP 'port' that
    SPI will be exposed.

--- rpi/main.c
+++ rpi/main.c
@@ -8,10 +8,52 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+static GMainLoop *main_loop;
+
+static unsigned int opt_port = 9000;
+
+static void sig_term(int sig)
+{
+	g_main_loop_quit(main_loop);
+}
+
+static GOptionEntry options[] = {
+	{ "port", 'p', 0, G_OPTION_ARG_INT, &opt_port, "port", "passthrough port" },
+	{ NULL },
+};
+
 
 int main(int argc, char *argv[])
 {
+	GOptionContext *context;
+	GError *gerr = NULL;
+
 	printf("RPi SPI passthrough over TCP\n");
+
+	context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, options, NULL);
+
+	if (!g_option_context_parse(context, &argc, &argv, &gerr)) {
+		printf("Invalid arguments: %s\n", gerr->message);
+		g_error_free(gerr);
+		g_option_context_free(context);
+		return EXIT_FAILURE;
+	}
+
+	g_option_context_free(context);
+
+	signal(SIGTERM, sig_term);
+	signal(SIGINT, sig_term);
+	signal(SIGPIPE, SIG_IGN);
+
+	main_loop = g_main_loop_new(NULL, FALSE);
+
+	g_main_loop_run(main_loop);
+
+	g_main_loop_unref(main_loop);
 
 	return 0;
 }
