46924b6565bbf19d0618b75af1d8b3ea3c6652b7
Claudio Takahasi <ckt@cesar.org.br>
2016-07-30 06:19:07 -0300

    driver: Remove unneeded empty driver definition
    Each client/server socket has a 'domain' and unique id allowing to
    map to its phy driver.

--- src/phy_driver.c
+++ src/phy_driver.c
@@ -13,65 +13,6 @@
 #include <Arduino.h>
 #endif
 
-#define EMPTY_DRIVER_NAME	"Empty driver"
-
-static phy_driver driver_empty;
-
-int empty_probe()
-{
-	return KNOT_NET_ERROR;
-}
-
-void empty_remove()
-{
-
-}
-
-int empty_socket()
-{
-	return KNOT_SOCKET_FD_INVALID;
-}
-
-void empty_close(int socket)
-{
-
-}
-
-int empty_connect(int socket, uint8_t to_addr)
-{
-	return KNOT_NET_ERROR;
-}
-
-int empty_generic_function(int socket)
-{
-	return KNOT_NET_ERROR;
-}
-
-size_t empty_recv (int sockfd, void *buffer, size_t len)
-{
-	return 0;
-}
-
-size_t empty_send (int sockfd, const void *buffer, size_t len)
-{
-	return 0;
-}
-
-phy_driver driver_empty = {
-	.name = EMPTY_DRIVER_NAME,
-	.valid = 0,
-	.probe = empty_probe,
-	.remove = empty_remove,
-	.socket = empty_socket,
-	.close = empty_close,
-	.listen = empty_generic_function,
-	.accept = empty_generic_function,
-	.connect = empty_connect,
-	.available = empty_generic_function,
-	.recv = empty_recv,
-	.send = empty_send
-};
-
 #ifdef HAVE_SERIAL
 extern phy_driver driver_serial;
 #endif
@@ -107,50 +48,34 @@ extern phy_driver driver_ask;
 phy_driver *drivers[] = {
 #ifdef HAVE_SERIAL
 	&driver_serial,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_NRF24
 	&driver_nrf24,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_NRF51
 	&driver_nrf51,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_NRF905
 	&driver_nrf905,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_RFM69
 	&driver_rfm69,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_ETH
 	&driver_eth,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_ESP8266
 	&driver_esp8266,
-#else
-	&driver_empty,
 #endif
 
 #ifdef HAVE_ASK
 	&driver_ask,
-#else
-	&driver_empty,
 #endif
 	NULL
 };
