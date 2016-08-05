58e050bdecdcc008b86ffb83c6060c6e8e1937bb
Claudio Takahasi <ckt@cesar.org.br>
2016-08-05 13:13:43 -0300

    nrf: Add initial source for nrfd
    This patch adds the initial source file for nrfd service. This daemon
    (Linux/RPi specific) will be responsible for managing nRF radio and
    forward all traffic to knotd through unix sockets.

---
+++ nrf/main.c
@@ -0,0 +1,81 @@
+/*
+ * This file is part of the KNOT Project
+ *
+ * Copyright (c) 2016, CESAR. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of the CESAR nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL CESAR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+static GMainLoop *main_loop;
+
+static const char *opt_host = NULL;
+static unsigned int opt_port = 9000;
+
+static void sig_term(int sig)
+{
+	g_main_loop_quit(main_loop);
+}
+
+static GOptionEntry options[] = {
+	{ "host", 'h', 0, G_OPTION_ARG_STRING, &opt_host,
+					"host", "Host exposing nRF24L01 SPI" },
+	{ "port", 'p', 0, G_OPTION_ARG_INT, &opt_port,
+					"port", "Remote port" },
+	{ NULL },
+};
+
+int main(int argc, char *argv[])
+{
+	GOptionContext *context;
+	GError *gerr = NULL;
+
+	printf("KNOT nrfd\n");
+
+	context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, options, NULL);
+
+	if (!g_option_context_parse(context, &argc, &argv, &gerr)) {
+		printf("Invalid arguments: %s\n", gerr->message);
+		g_error_free(gerr);
+		g_option_context_free(context);
+		return EXIT_FAILURE;
+	}
+
+	g_option_context_free(context);
+
+	signal(SIGTERM, sig_term);
+	signal(SIGINT, sig_term);
+	signal(SIGPIPE, SIG_IGN);
+
+	main_loop = g_main_loop_new(NULL, FALSE);
+
+	g_main_loop_run(main_loop);
+
+	g_main_loop_unref(main_loop);
+
+	return 0;
+}
