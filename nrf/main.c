8676e93c0ce7aca257ba0fc34e622e0b37f2f5c5
Claudio Takahasi <ckt@cesar.org.br>
2016-08-08 08:51:53 -0300

    nrf: Add connecting to RPi nRF proxy
    This patch adds the TCP connection to read raw SPI data sent by
    RPi nRF proxy/passthrough (rpiptd).

--- nrf/main.c
+++ nrf/main.c
@@ -7,8 +7,15 @@
  *
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 #include <glib.h>
 
 static GMainLoop *main_loop;
@@ -19,6 +26,125 @@ static unsigned int opt_port = 9000;
 static void sig_term(int sig)
 {
 	g_main_loop_quit(main_loop);
+}
+
+static gboolean nrf_data_watch(GIOChannel *io, GIOCondition cond,
+						gpointer user_data)
+{
+	char buffer[1024];
+	GIOStatus status;
+	GError *gerr = NULL;
+	gsize rbytes;
+
+	/*
+	 * Manages TCP data from rpiptd(nRF proxy). All traffic(raw
+	 * data) should be transferred using unix socket to knotd.
+	 */
+
+	if (cond & (G_IO_HUP | G_IO_ERR))
+		return FALSE;
+
+	memset(buffer, 0, sizeof(buffer));
+
+	/* Incoming data through TCP socket */
+	status = g_io_channel_read_chars(io, buffer, sizeof(buffer),
+						 &rbytes, &gerr);
+	if (status == G_IO_STATUS_ERROR) {
+		printf("read(): %s\n", gerr->message);
+		g_error_free(gerr);
+		return FALSE;
+	}
+
+	if (rbytes == 0)
+		return FALSE;
+
+	/*
+	 * Decode based on nRF PIPE information and forward
+	 * the data through a unix socket to knotd.
+	 */
+	printf("read(): %lu bytes\n", rbytes);
+
+	return TRUE;
+}
+
+static int tcp_init(void)
+{
+	struct hostent *host;			/* Host information */
+	struct in_addr h_addr;			/* Internet address */
+	struct sockaddr_in server;		/* nRF proxy: rpiptd */
+	int err, sock;
+
+	host = gethostbyname(opt_host);
+	if (host == NULL) {
+		err = errno;
+		fprintf(stderr, "gethostbyname(): %s(%d)\n",
+						strerror(err), err);
+		return -err;
+	}
+
+	h_addr.s_addr = *((unsigned long *) host-> h_addr_list[0]);
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock < 0) {
+		err = errno;
+		fprintf(stderr, "socket(): %s(%d)\n", strerror(err), err);
+		return -err;
+	}
+
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = h_addr.s_addr;
+	server.sin_port = htons(opt_port);
+
+	err = connect(sock, (struct sockaddr *) &server, sizeof(server));
+	if (err < 0) {
+		err = errno;
+		fprintf(stderr, "connect(): %s(%d)\n", strerror(err), err);
+		close(sock);
+		return -err;
+	}
+
+	fprintf(stdout, "nRF Proxy address: %s\n", inet_ntoa(h_addr));
+
+	return sock;
+}
+
+static int radio_init(void)
+{
+	GIOChannel *io;
+	GIOCondition cond = G_IO_IN | G_IO_ERR | G_IO_HUP;
+	int sock;
+
+	if (opt_host == NULL) {
+		/*
+		 * TODO: add RPi SPI init
+		 */
+		return -ENOSYS;
+	} else {
+		/*
+		 * TCP development mode: Linux connected to RPi(nrfd radio
+		 * proxy). Connect to nrfd routing all traffic over TCP.
+		 */
+		sock = tcp_init();
+	}
+
+	if (sock < 0)
+		return -EIO;
+
+	io = g_io_channel_unix_new(sock);
+	g_io_channel_set_close_on_unref(io, TRUE);
+
+	/* Ending 'NULL' for binary data */
+	g_io_channel_set_encoding(io, NULL, NULL);
+	g_io_channel_set_buffered(io, FALSE);
+
+	/* TCP handler: incoming data from rpiptd (nRF proxy) */
+	g_io_add_watch(io, cond, nrf_data_watch, NULL);
+
+	/* Keep only one reference: watch */
+	g_io_channel_unref(io);
+
+	return 0;
 }
 
 static GOptionEntry options[] = {
@@ -33,6 +159,7 @@ int main(int argc, char *argv[])
 {
 	GOptionContext *context;
 	GError *gerr = NULL;
+	int err;
 
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, options, NULL);
@@ -57,6 +184,12 @@ int main(int argc, char *argv[])
 		printf("Development mode: %s:%u\n", opt_host, opt_port);
 	else
 		printf("Native SPI mode\n");
+
+	err = radio_init();
+	if (err < 0) {
+		g_main_loop_unref(main_loop);
+		return EXIT_FAILURE;
+	}
 
 	g_main_loop_run(main_loop);
 
