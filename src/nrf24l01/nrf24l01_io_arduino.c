6ac96246956d44f5dfd275043be45bc40c1b11c1
Rebeca Alencar <rvsa@cesar.org.br>
2016-08-25 09:42:14 -0300

    nrf24l01: Create the file nrf24l01_io to works with arduino.
    This patchs is the initial commit to nrf24l01_io to arduino.

---
+++ src/nrf24l01/nrf24l01_io_arduino.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+#include <Arduino.h>
+#include <stdint.h>
+#include "nrf24l01_io.h"
+#include "spi.h"
+
+#define CE	1
+
+void delay_us(float us)
+{
+
+}
+
+void delay_ms(float ms)
+{
+
+}
+
+void enable(void)
+{
+
+}
+
+void disable(void)
+{
+
+}
+
+void io_setup(void)
+{
+
+}
+
+void io_reset(void)
+{
+
+}
