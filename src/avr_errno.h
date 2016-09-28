181a56b7a567b44e85da59d9237ffd3533ebd4c1
Heitor Fonseca <hfa@cesar.org.br>
2016-09-28 11:37:23 -0300

    avr_errno: Add errno header file for Arduino.
    This patch adds a errno header. It defines defines a list of macros
    indicating different and unique error codes.

---
+++ src/avr_errno.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#define	EINVAL			22	/* Invalid argument */
+#define EAGAIN			11	/* Resource temporarily unavailable */
