574e826a382f8ffdbab62e32dfa10aa82a003f6c
Erick Simões <erick.simoes@cesar.org.br>
2016-10-05 13:37:03 -0300

    log: Implementation of log lib for Linux (RPI)
    Implementation of lib responsible for the system log in Linux and
    modification of necessary configuration files. Logs categorized as
    'info', 'warn' and 'error' will be sent via the serial port.

---
+++ src/hal/log/log_linux.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <locale.h>
+
+#include "include/log.h"
+
+#define SIZE_CHAR_BUF 10
+
+static int fd;
+
+int hal_log_open(const char *pathname)
+{
+	setlocale(LC_ALL, "");
+
+	fd = open(pathname, O_CREAT | O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return -errno;
+
+	return 0;
+}
+
+void logger(const char *file, const char *function, long line,
+				const char *args, const char *category)
+{
+	int len = strlen(file) + strlen(function) + strlen(args) +
+					strlen(category) + SIZE_CHAR_BUF + 1;
+	char buf[len];
+
+	snprintf(buf, len, "%s%s::%s(%lu):%s\n", category, file, function,
+								line, args);
+
+	write(fd, buf, strlen(buf));
+}
+
+void hal_log_close(void)
+{
+	close(fd);
+}
