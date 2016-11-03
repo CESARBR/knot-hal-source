5461f744ea475e05c5dc442679bec7d3a5439b71
Claudio Takahasi <ckt@cesar.org.br>
2016-11-03 10:20:52 -0300

    comm: Remove management 'socket'
    Management 'socket' used to access the local adapter will be replaced
    by hal_comm_open(). The identifier returned can be used to read events
    such as discovered devices or new connections established.

--- include/comm.h
+++ include/comm.h
@@ -11,22 +11,32 @@
 #define HAL_COMM_PF_NRF24		1
 
 /*
- * MGMT: adapter setup and link layer generated events
  * UDATA: Sequenced and reliable protocol
  */
-#define HAL_COMM_NRF24PROTO_MGMT	64
-#define HAL_COMM_NRF24PROTO_UDATA	65
+#define HAL_COMM_NRF24PROTO_UDATA	64
 
 /* Serial port */
 int hal_comm_open(const char *pathname);
 
-/* nRF24 and other radios */
+/*
+ * nRF24 and other radios. Returns -ENFILE (limit of resources/pipes has
+ * been reached) or an id representing the logical communication channel
+ * for a new client.
+ */
 int hal_comm_socket(int domain, int protocol);
 
 void hal_comm_close(int sockfd);
 
+/* Non-blocking read operation. Returns -EGAIN if there isn't data available */
 ssize_t hal_comm_read(int sockfd, void *buffer, size_t count);
+
+/* Blocking write operation. Returns -EBADF if not connected */
 ssize_t hal_comm_write(int sockfd, const void *buffer, size_t count);
 
 int hal_comm_listen(int sockfd);
+
+/* Non-blocking operation. Returns -EGAIN if there isn't a new client */
 int hal_comm_accept(int sockfd, uint64_t *addr);
+
+/* Blocking operation. Returns -ETIMEOUT */
+int hal_comm_connect(int sockfd, uint64_t *addr);
