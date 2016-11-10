577a7b4691122920dd1c0520b868cdb049b9d29f
Claudio Takahasi <ckt@cesar.org.br>
2016-11-09 23:50:04 -0300

    comm: Add HAL comm serial skeleton
    This commit adds the initial file used as entry point to define
    platform independent communication over serial ports.

---
+++ src/hal/comm/comm_serial.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#ifdef ARDUINO
+#include <avr_errno.h>
+#include <avr_unistd.h>
+#else
+#include <errno.h>
+#include <unistd.h>
+#endif
+
+#include <stdint.h>
+
+#include "include/comm.h"
+
+int hal_comm_socket(int domain, int protocol)
+{
+	return -ENOSYS;
+}
+
+void hal_comm_close(int sockfd)
+{
+}
+
+ssize_t hal_comm_read(int sockfd, void *buffer, size_t count)
+{
+	return -ENOSYS;
+}
+
+ssize_t hal_comm_write(int sockfd, const void *buffer, size_t count)
+{
+	return -ENOSYS;
+}
+
+int hal_comm_listen(int sockfd)
+{
+	/* It is not applied to serial ports */
+
+	return -ENOSYS;
+}
+
+int hal_comm_accept(int sockfd, uint64_t *addr)
+{
+	/* It is not applied to serial ports */
+
+	return -ENOSYS;
+}
+
+int hal_comm_connect(int sockfd, uint64_t *addr)
+{
+	/* It is not applied to serial ports */
+
+	return -ENOSYS;
+}
