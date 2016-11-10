7b36fc54642912327c3ccbcaf1cf98e326771af2
Claudio Takahasi <ckt@cesar.org.br>
2016-11-09 23:37:42 -0300

    Fix copyright
    This patch fixes copyright for physerial.c and nrf24l01_io_linux.c files.

--- src/nrf24l01/nrf24l01_io_linux.c
+++ src/nrf24l01/nrf24l01_io_linux.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdint.h>
