d9b37b164989f370a7c2f4946dd94f4ddace68e8
Rebeca Alencar <rvsa@cesar.org.br>
2016-11-11 11:05:46 -0300

    Fix: set ARD to be different for each pipe
    To reduce data collisions each pipe have different ARD value.
    ARD (Auto Retransmit Delay) is the time to start retransmission.

--- src/nrf24l01/nrf24l01.c
+++ src/nrf24l01/nrf24l01.c
@@ -367,14 +367,12 @@ int8_t nrf24l01_set_ptx(int8_t spi_fd, uint8_t pipe)
 	set_address_pipe(spi_fd, NRF24_TX_ADDR, get_address_pipe(spi_fd, pipe));
 	#if (NRF24_ARC != NRF24_ARC_DISABLE)
 		/*
-		* Set ARC and ARD retry periods
-		* ARD range is 1500us <= ARD <= 4000us
-		* the function NRF24_RETR_ARD receives v which
-		* 0 <= v <= 15 and
-		* 0 represents 250us and 15 represents 4000us
+		* Set ARC and ARD by pipe index to different
+		* retry periods to reduce data collisions
+		* compute ARD range: 1500us <= ARD[pipe] <= 4000us
 		*/
 		outr(spi_fd, NRF24_SETUP_RETR,
-			NRF24_RETR_ARD(5) /*1000us */
+			NRF24_RETR_ARD(((pipe * 2) + 5))
 			| NRF24_RETR_ARC(NRF24_ARC));
 	#endif
 	outr(spi_fd, NRF24_STATUS, NRF24_ST_TX_DS | NRF24_ST_MAX_RT);
