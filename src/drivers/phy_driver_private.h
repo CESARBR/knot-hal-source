21bc40d5c8f0e7d35a9ccdd7dfc21b497fd2615f
Heitor Fonseca <hfa@cesar.org.br>
2016-11-15 00:48:08 -0300

    Fix: remove unusable functions declarations

--- src/drivers/phy_driver_private.h
+++ src/drivers/phy_driver_private.h
@@ -14,12 +14,12 @@
  /**
  * struct phy_driver - driver abstraction for the physical layer
  * @name: driver name
- * @probe: function to initialize the driver
- * @remove: function to close the driver and free its resources
- * @socket: function to create a new socket FD
- * @listen: function to enable incoming connections
- * @accept: function that waits for a new connection and returns a 'pollable' FD
- * @connect: function to connect to a server socket
+ * @pathname: driver path
+ * @open: function to initialize the driver
+ * @close: function to close the driver and free its resources
+ * @ioctl: function to device-specific input/output operations
+ * @ref_open: reference driver open
+ * @fd: driver fd
  *
  * This 'driver' intends to be an abstraction for Radio technologies or
  * proxy for other services using TCP or any socket based communication.
@@ -28,14 +28,9 @@
 struct phy_driver {
 	const char *name;
 	const char *pathname;
-	int (*probe) (const char *spi, uint8_t tx_pwr);
-	void (*remove) (void);
 
 	int (*open) (const char *pathname);
 	void (*close) (int sockfd);
-	int (*listen) (int srv_sockfd, uint8_t channel);
-	int (*accept) (int srv_sockfd);
-	int (*connect) (int cli_sockfd, uint8_t to_addr);
 	ssize_t (*recv) (int sockfd, void *buffer, size_t len);
 	ssize_t (*send) (int sockfd, const void *buffer, size_t len);
 	int (*ioctl) (int sockfd, int cmd, void *arg);
