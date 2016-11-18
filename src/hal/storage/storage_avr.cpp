898a99b6b9f7273e290a06aed05b2202e566e084
Douglas Vasconcelos <dssv@cesar.org.br>
2016-11-18 13:34:56 -0300

    Add new function to storage in the end of EEPROM
    To avoid misuse of the developer, configuration information is stored
    at the end of eeprom, a parameter is passed to identify which data will
    be stored.

--- src/hal/storage/storage_avr.cpp
+++ src/hal/storage/storage_avr.cpp
@@ -6,9 +6,21 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
-
 #include <avr/eeprom.h>
 #include "storage.h"
+
+// Address where each data is stored at the end of EEPROM
+#define ADDR_UUID		0
+#define ADDR_TOKEN		40
+#define ADDR_SIZE_CONFIG	76
+#define ADDR_CONFIG		78
+
+// Identifier of data type to be stored
+#define DATA_UUID		0
+#define DATA_TOKEN		1
+#define DATA_CONFIG		2
+
+//TODO: Fix parameter order (data, len , addr)
 
 int hal_storage_read(uint16_t addr, uint8_t *value, uint16_t len)
 {
@@ -38,4 +50,36 @@ int hal_storage_write(uint16_t addr, const uint8_t *value, uint16_t len)
 	}
 
 	return i;
+}
+
+
+int hal_storage_write_end(void *value, uint16_t len, uint8_t data)
+{
+	/* Position where the data will be stored */
+	int dst;
+
+	/*
+	 * Calculate different addresses to store the
+	 * value according to the parameter passed.
+	 */
+	switch (data) {
+	case DATA_UUID:
+		dst = EE2END - len - ADDR_UUID;
+		break;
+	case DATA_TOKEN:
+		dst = EE2END - len - ADDR_TOKEN;
+		break;
+	case DATA_CONFIG:
+		dst = EE2END - len - ADDR_CONFIG;
+		/* Store the size of the config to know where it end in the EEPROM */
+		hal_storage_write(EE2END - ADDR_SIZE_CONFIG, len, 2);
+		break;
+	default:
+		return -1;
+	}
+
+	/* Store all the block in the calculated position */
+	eeprom_write_block(value, dst, len);
+
+	return len;
 }
