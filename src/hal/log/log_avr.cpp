e77705ca5f48227a2e3886403d1073e8ea4a8615
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2016-11-21 18:26:13 -0300

    Adding parameter to choose the serial used to log
    In ArduinoMega is possible to choose Serial1, Serial2 and Serial3.
    For that is used a String contaning Serial, Serial1, Seria2 or Serial3 as
    parameter in hal_log_open. Serial is the default opition.

--- src/hal/log/log_avr.cpp
+++ src/hal/log/log_avr.cpp
@@ -9,54 +9,78 @@
 
 #include <Arduino.h>
 
+#include "avr_errno.h"
 #include "log.h"
 
 #define SERIAL_DATA_RATE 9600
 
 static bool status_enabled =  false;
 
+static HardwareSerial* _serial;
+
 int hal_log_open(const char *pathname)
 {
-	/*
-	 * TODO: The pathname variable will be used
-	 * to set the serial port of the Arduino
-	 */
+	if (strncmp("serial", pathname, 6) != 0)
+		return -EINVAL;
+
+	if (strlen(pathname) != 7)
+		return -EINVAL;
+
+	switch(pathname[6]) {
+#ifdef SERIAL_PORT_HARDWARE1
+	case '1':
+		_serial = &Serial1;
+		break;
+#endif
+
+#ifdef SERIAL_PORT_HARDWARE2
+	case '2':
+		_serial = &Serial2;
+		break;
+#endif
+
+#ifdef SERIAL_PORT_HARDWARE3
+	case '3':
+		_serial = &Serial3;
+		break;
+#endif
+	default:
+		return -EINVAL;
+	}
 
 	if (!status_enabled) {
-		Serial.begin(SERIAL_DATA_RATE);
+		_serial->begin(SERIAL_DATA_RATE);
 
 		status_enabled = true;
-		Serial.print("Serial communication enabled (");
+		_serial->print("Serial communication enabled (");
 	} else {
-		Serial.print("Serial communication already enabled (");
+		_serial->print("Serial communication already enabled (");
 	}
-	Serial.print(SERIAL_DATA_RATE);
-	Serial.println(")");
-
+	_serial->print(SERIAL_DATA_RATE);
+	_serial->println(")");
 	return 0;
 }
 
 void logger(const char *file, const char *function, long line,
 				const char *args, const char *category)
 {
-	Serial.print(category);
-	Serial.print(file);
-	Serial.print("::");
-	Serial.print(function);
-	Serial.print("(");
-	Serial.print(line);
-	Serial.print("): ");
-	Serial.println(args);
+	_serial->print(category);
+	_serial->print(file);
+	_serial->print("::");
+	_serial->print(function);
+	_serial->print("(");
+	_serial->print(line);
+	_serial->print("): ");
+	_serial->println(args);
 }
 
 void hal_log_close(void)
 {
 	if (status_enabled) {
-		Serial.print("Serial communication disabled (");
-		Serial.print(SERIAL_DATA_RATE);
-		Serial.println(")");
-		Serial.end();
+		_serial->print("Serial communication disabled (");
+		_serial->print(SERIAL_DATA_RATE);
+		_serial->println(")");
+		_serial->end();
 		status_enabled = false;
 	}
-
 }
