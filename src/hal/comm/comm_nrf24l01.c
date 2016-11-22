f7435699b27a9c93e7a9a2a484d6e9d3d7955e6d
Heitor Fonseca <hfa@cesar.org.br>
2016-11-22 10:03:04 -0300

    Fix: change socket index
    Change sockIndex initialization to index correctly the peers.

--- src/hal/comm/comm_nrf24l01.c
+++ src/hal/comm/comm_nrf24l01.c
@@ -425,7 +425,8 @@ static void running(void)
 {
 
 	static int state = 0;
-	static int sockIndex = 0; /* Index peers */
+	/* Index peers */
+	static int sockIndex = 1;
 	static unsigned long start;
 
 	switch (state) {
@@ -467,15 +468,15 @@ static void running(void)
 		if (hal_timeout(hal_time_ms(), start, 60) > 0)
 			state = 0;
 
-		if (sockIndex == CONNECTION_COUNTER)
-			sockIndex = 0;
+		/* Resets sockIndex if sockIndex > CONNECTION_COUNTER */
+		if (sockIndex > CONNECTION_COUNTER)
+			sockIndex = 1;
 
 		/* Check if pipe is allocated */
-		if (peers[sockIndex].pipe != -1) {
+		if (peers[sockIndex-1].pipe != -1) {
 			write_raw(driverIndex, sockIndex);
 			read_raw(driverIndex, sockIndex);
 		}
-
 
 		sockIndex++;
 		break;
