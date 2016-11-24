7d3f14af01a60bbe4c280bdc3c447eada5aa6be2
Tiago Barros <tiago.barros@cesar.org.br>
2016-11-24 16:18:19 -0300

    FIX: add support to default Serial (Serial0) on arduino

--- src/hal/log/log_avr.cpp
+++ src/hal/log/log_avr.cpp
@@ -12,51 +12,62 @@
 #include "avr_errno.h"
 #include "log.h"
 
-#define SERIAL_DATA_RATE 9600
+#define SERIAL_BAUD_RATE	9600
+#define LOG_BUFFER_LEN		50
 
 static bool status_enabled =  false;
 
-static HardwareSerial* _serial;
+#ifdef LEONARDO // Arduino Leonardo
+static Serial_* _serial = &Serial;
+#else
+static HardwareSerial* _serial = &Serial;
+#endif
 
 int hal_log_open(const char *pathname)
 {
 	if (strncmp("serial", pathname, 6) != 0)
 		return -EINVAL;
 
-	if (strlen(pathname) != 7)
-		return -EINVAL;
-
-	switch(pathname[6]) {
-#ifdef SERIAL_PORT_HARDWARE1
-	case '1':
-		_serial = &Serial1;
-		break;
+	if (strlen(pathname) == 7) {
+		switch(pathname[6]) {
+#ifdef HAVE_HWSERIAL0
+		case '0':
+			_serial = &Serial;
+			break;
 #endif
 
-#ifdef SERIAL_PORT_HARDWARE2
-	case '2':
-		_serial = &Serial2;
-		break;
+#ifdef HAVE_HWSERIAL1
+		case '1':
+			_serial = &Serial1;
+			break;
 #endif
 
-#ifdef SERIAL_PORT_HARDWARE3
-	case '3':
-		_serial = &Serial3;
-		break;
+#ifdef HAVE_HWSERIAL2
+		case '2':
+			_serial = &Serial2;
+			break;
 #endif
-	default:
-		return -EINVAL;
+
+#ifdef HAVE_HWSERIAL3
+		case '3':
+			_serial = &Serial3;
+			break;
+#endif
+		default:
+			return -EINVAL;
+		}
 	}
 
+
 	if (!status_enabled) {
-		_serial->begin(SERIAL_DATA_RATE);
+		_serial->begin(SERIAL_BAUD_RATE);
 
 		status_enabled = true;
 		_serial->print("Serial communication enabled (");
 	} else {
 		_serial->print("Serial communication already enabled (");
 	}
-	_serial->print(SERIAL_DATA_RATE);
+	_serial->print(SERIAL_BAUD_RATE);
 	_serial->println(")");
 	return 0;
 }
@@ -64,7 +75,7 @@ int hal_log_open(const char *pathname)
 void logger(const char *file, const char *function, long line,
 		const char *category, const char *format, ...)
 {
-	char buf[50] = {0};
+	char buf[LOG_BUFFER_LEN] = {0};
 	va_list args;
 
 	_serial->print(category);
@@ -76,7 +87,7 @@ void logger(const char *file, const char *function, long line,
 	_serial->print("): ");
 
 	va_start(args, format);
-	vsnprintf(buf, 50, format, args);
+	vsnprintf(buf, LOG_BUFFER_LEN, format, args);
 	va_end(args);
 
 	_serial->println(buf);
@@ -86,7 +97,7 @@ void hal_log_close(void)
 {
 	if (status_enabled) {
 		_serial->print("Serial communication disabled (");
-		_serial->print(SERIAL_DATA_RATE);
+		_serial->print(SERIAL_BAUD_RATE);
 		_serial->println(")");
 		_serial->end();
 		status_enabled = false;
