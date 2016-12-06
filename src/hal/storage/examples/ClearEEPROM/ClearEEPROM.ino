c885e525b310a7626665c990ccb9dea7f4904374
Erick Simões <erick.simoes@cesar.org.br>
2016-12-06 17:10:19 -0300

    Add Arduino sketch to erase all EEPROM data

---
+++ src/hal/storage/examples/ClearEEPROM/ClearEEPROM.ino
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include "KNoTThing.h"
+#include "include/log.h"
+#include <EEPROM.h>
+
+void setup()
+{
+	hal_log_open("serial0");
+	hal_log_info("Setting the EEPROM addresses to zero...", 1);
+
+	for (int i = 0 ; i < EEPROM.length() ; i++) {
+		EEPROM.write(i, 0);
+	}
+
+	hal_log_info("Completed", 1);
+}
+
+void loop() {}
