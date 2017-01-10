a03e89be304bf738396aec3a78d4fbb317d16262
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2017-01-10 00:19:23 -0300

    comm_serial: Add init/deinit functions

--- src/hal/comm/comm_serial.c
+++ src/hal/comm/comm_serial.c
@@ -22,6 +22,16 @@
 
 #include "include/comm.h"
 
+int hal_comm_init(const char *pathname, const struct nrf24_mac *mac)
+{
+	return -ENOSYS;
+}
+
+int hal_comm_deinit(void)
+{
+	return -ENOSYS;
+}
+
 int hal_comm_socket(int domain, int protocol)
 {
 	return -ENOSYS;
