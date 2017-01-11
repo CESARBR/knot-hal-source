2d964865db55b53d10f40f5a5de021b4c43b1f74
Claudio Takahasi <ckt@cesar.org.br>
2017-01-11 16:11:34 -0300

    LoRa: Add initial source file for LoRa
    This patch adds the initial sources to implement a new daemon
    responsible for managing Semtech LoRa radio communication.

---
+++ src/lorad/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016-2017, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("LoRa daemon\n");
+
+	return 0;
+}
