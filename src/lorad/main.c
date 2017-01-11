e6af05a40d84352b2a662dee502e15e3de08e05a
Claudio Takahasi <ckt@cesar.org.br>
2017-01-11 16:31:35 -0300

    LoRa: Add glib mainloop to lorad

--- src/lorad/main.c
+++ src/lorad/main.c
@@ -8,10 +8,26 @@
  */
 
 #include <stdio.h>
+#include <glib.h>
+
+static GMainLoop *main_loop;
+
+static void sig_term(int sig)
+{
+	g_main_loop_quit(main_loop);
+}
 
 int main(int argc, char *argv[])
 {
 	printf("LoRa daemon\n");
+
+	signal(SIGTERM, sig_term);
+	signal(SIGINT, sig_term);
+	signal(SIGPIPE, SIG_IGN);
+
+	main_loop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(main_loop);
+	g_main_loop_unref(main_loop);
 
 	return 0;
 }
