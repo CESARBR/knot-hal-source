f303b5058b6d47e91a4eeec0a8552940c1d354c6
Claudio Takahasi <ckt@cesar.org.br>
2017-06-15 14:05:20 -0300

    nRF24: Reduce keep alive interval to 512ms

--- src/hal/comm/nrf24l01_ll.h
+++ src/hal/comm/nrf24l01_ll.h
@@ -101,7 +101,7 @@ struct nrf24_ll_crtl_pdu {
 } __attribute__ ((packed));
 
 
-#define NRF24_KEEPALIVE_SEND_MS		2560
+#define NRF24_KEEPALIVE_SEND_MS		512
 #define NRF24_KEEPALIVE_TIMEOUT_MS	(10 * NRF24_KEEPALIVE_SEND_MS)
 
 /*
