29d867c1da2f5de20bd37d682761ff041081b6f3
Vitor Barros <vba@cesar.org.br>
2017-08-04 14:32:37 -0300

    LoRa: Add initial sx127x hal file

---
+++ src/lora/sx127x_hal_linux.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+#include "sx127x_hal.h"
+
+
+void hal_init(void)
+{
+
+}
+
+void hal_pin_nss(uint8_t val)
+{
+
+}
+
+void hal_pin_rxtx(uint8_t val)
+{
+
+}
+
+void hal_pin_rst(uint8_t val)
+{
+
+}
+
+uint8_t hal_spi(uint8_t outval)
+{
+	return 0;
+}
+
+void hal_disableIRQs(void)
+{
+
+}
+
+void hal_enableIRQs(void)
+{
+
+}
+
+void hal_sleep(void)
+{
+
+}
+
+uint32_t hal_ticks(void)
+{
+	return 0;
+}
+
+void hal_waitUntil(uint32_t time)
+{
+
+}
+
+uint8_t hal_checkTimer(uint32_t targettime)
+{
+	return 0;
+}
+
+void hal_failed(void)
+{
+
+}
