124f082dbe43a257ad7b49e992fac740562a8bf9
Helmiton Moraes da Silva Cunha Jr <hmscj@cesar.org.br>
2017-11-07 14:19:44 -0300

    nRF24: Add extern "C" to phy_driver.h
    Extern "C" is necessary for low level Arduino applications using
    radio PHY API directly.

--- src/drivers/phy_driver.h
+++ src/drivers/phy_driver.h
@@ -7,8 +7,16 @@
  *
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int phy_open(const char *pathname);
 int phy_close(int sockfd);
 ssize_t phy_read(int sockfd, void *buffer, size_t len);
 ssize_t phy_write(int sockfd, const void *buffer, size_t len);
 int phy_ioctl(int sockfd, int cmd, void *arg);
+
+#ifdef __cplusplus
+}
+#endif
